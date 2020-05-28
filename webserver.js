b460fdcae7779af16778976f81852848ae44493c
Lisa Schulzke <lisa.schulzke@hfg-gmuend.de>
2020-05-28 20:39:34 +0200

    did connection to mongodb

--- webserver.js
+++ webserver.js
@@ -1,22 +1,39 @@
+//Requires / import libraries
 let express = require('express');
 let axios = require('axios').default
 const cors = require('cors');
+const mongo = require('mongodb').MongoClient;
+
+//Variables
 let app = express();
 const bodyParser = require('body-parser');
-app.use(bodyParser.json());
-app.use(cors());
 let uri = 'https://gist.githubusercontent.com/fg-uulm/666847dd7f11607fc2b6234c6d84d188/raw/2ca994ada633143903b10b2bf7ada3fd039cae35/mensa.json';
 let data = '';
+let url = "mongodb://localhost:27017/mydb";
+
+
+app.use(bodyParser.json());
+app.use(cors());
+
+mongo.connect(url, function(err, db) {
+  if (err) throw err;
+  let dbo = db.db("databaseMensa");
+  let myObj = {name: "Pizza", day:"Mi", category:"H1"};
+  dbo.collection("meals").insertOne(myObj, function(err, res) {
+    if (err) throw err;
+    console.log("1 documented inserted!");
+    db.close();
+  });
+});
+
 
 axios({
   method: 'get',
   url: uri,
   responseType: 'json',
-})
-
-.then((response) => {
+}).then((response) => {
   const output = response.data;
-  console.log(output);
+  //console.log(output);
   data = output;
 })
 
