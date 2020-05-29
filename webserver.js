5f4b8c854ed8978d65b054fba745aea71a50e8b3
Lisa Schulzke <lisa.schulzke@hfg-gmuend.de>
2020-05-29 09:01:20 +0200

    data saved to object in database

--- webserver.js
+++ webserver.js
@@ -10,6 +10,7 @@ const bodyParser = require('body-parser');
 let uri = 'https://gist.githubusercontent.com/fg-uulm/666847dd7f11607fc2b6234c6d84d188/raw/2ca994ada633143903b10b2bf7ada3fd039cae35/mensa.json';
 let data = '';
 let url = "mongodb://localhost:27017/mydb";
+let database;
 
 
 app.use(bodyParser.json());
@@ -17,13 +18,13 @@ app.use(cors());
 
 mongo.connect(url, function(err, db) {
   if (err) throw err;
-  let dbo = db.db("databaseMensa");
+  database = db.db("databaseMensa");
   let myObj = {name: "Pizza", day:"Mi", category:"H1"};
-  dbo.collection("meals").insertOne(myObj, function(err, res) {
+  database.collection("meals").insertOne(myObj, function(err, res) {
     if (err) throw err;
     console.log("1 documented inserted!");
-    db.close();
   });
+
 });
 
 
@@ -39,21 +40,30 @@ axios({
 
 app.get('/mensa/:day', (req, res) => {
   if (req.params.day === 'Di') {
-    res.send(data);
+    let data = {data: "data"}
+    database.collection("meals").findOne(data, function(err, result) {
+      if(err) throw err;
+      console.log(result);
+      res.send(result);
+    })
   } else {
     res.status(404).send('404');
   }
 });
 
-let postedData = [];
 
-app.post('/form_data', (req,res,next)=>{
-  postedData.push(req.body)
-  res.send("OK");
+app.post('/mensa/insert',(req, res, next) =>{
+  let data = {data: req.body};
+  database.collection("meals").insertOne(data, function(err, res) {
+    if (err) throw err;
+    console.log("1 documented inserted!");
+    // database.close();
+  });
+  res.status(200).send('OK');
 });
 
 
-//Server starten
+//start server
 app.listen(3000, function () {
     console.log('Example app listening on port 3000!');
 });
