ccffd2e3c8c0a761bca6d0f2d4d67cbbb051e8db
Lisa Schulzke <lisa.schulzke@hfg-gmuend.de>
2020-06-01 13:34:55 +0200

    changed structure , added better post and get re

--- webserver.js
+++ webserver.js
@@ -16,14 +16,14 @@ let database;
 app.use(bodyParser.json());
 app.use(cors());
 
-mongo.connect(url, function(err, db) {
+mongo.connect(url, function (err, db) {
   if (err) throw err;
   database = db.db("databaseMensa");
-  let myObj = {name: "Pizza", day:"Mi", category:"H1"};
-  database.collection("meals").insertOne(myObj, function(err, res) {
-    if (err) throw err;
-    console.log("1 documented inserted!");
-  });
+  // let myObj = {name: "Pizza", day:"Mi", category:"H1"};
+  // database.collection("meals").insertOne(myObj, function(err, res) {
+  //   if (err) throw err;
+  //   console.log("1 documented inserted!");
+  // });
 
 });
 
@@ -38,38 +38,71 @@ axios({
   data = output;
 })
 
-app.get('/mensa/:day', (req, res) => {
-  if (req.params.day === 'Di') {
-    let data = {name: "Pizza"}
-    database.collection("meals").findOne(data, function(err, result) {
-      if(err) throw err;
-      console.log(result);
-      res.send(result);
-    })
-  } else {
-    res.status(404).send('404');
+app.get('/mensa/:day', async (req, res) => {
+  //if no category, many meals appear with findMany()
+  let key = {
+    day: req.body.day
   }
+  // let result = await database.collection("meals").findOne(key)
+  let result = await database.collection("meals").find(key)
+  // result.forEach(item => console.log) 
+  //console.log("get", result);
+  if (!result) {
+    res.status(401).send("Error");
+  }
+  let myDocument;
+  let docs = [];
+  while (result.hasNext()) {
+    myDocument = await result.next();
+    if (myDocument) {
+      console.log("CURSORDATEN", myDocument);
+      docs.push();
+    }
+  }
+
+
+
+  //if this object is filled, console prints result
+
+
+  res.status(200).send("OK");
 });
 
 
-app.post('/mensa',async(req, res, next) =>{
-  let data = {data: req.body};
-  let key = {day:req.body.day, category:req.body.category}
-  result = await database.collection("meals").findOne(key, function(err, result) {
-    if(err) throw err;
-    console.log(result);
-    res.send(result);
-  })
-  database.collection("meals").insertOne(data, function(err, res) {
-    if (err) throw err;
-    console.log("1 documented inserted!");
-    // database.close();
+app.post('/mensa', async (req, res, next) => {
+  let data = req.body;
+  console.log("Daten:", data);
+  let key = {
+    day: req.body.day,
+    category: req.body.category
+  }
+  let result = await database.collection("meals").findOne({
+    ...key
   });
+  console.log("Database findone", result);
+  if (!result) {
+    result = await database.collection("meals").insertOne(data);
+    console.log("insert one", result.ops)
+  } else {
+    let updated = {
+      ...result,
+      ...req.body
+    }
+    result = await database.collection("meals").updateOne({
+      _id: result._id
+    }, {
+      $set: {
+        ...updated
+      }
+    });
+    console.log("update one", result.message.documents)
+  }
+
   res.status(200).send('OK');
 });
 
 
 //start server
 app.listen(3000, function () {
-    console.log('Example app listening on port 3000!');
+  console.log('Example app listening on port 3000!');
 });
